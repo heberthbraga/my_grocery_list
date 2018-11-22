a0dca135cb769e330f37f3aa6b15def8a9d17e18
Heberth Braga <braga.heberth@gmail.com>
2018-11-22 10:56:06 -0300

    Change carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -22,6 +22,14 @@ CarrierWave.configure do |config|
   else
     config.aws_bucket = ENV.fetch('AWS_BUCKET_NAME')
     config.storage    = :aws
+
+    # The maximum period for authenticated_urls is only 7 days
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+    config.aws_attributes = {
+        expires: 1.week.from_now.httpdate,
+        cache_control: 'max-age=315576000'
+    }
     
     config.aws_credentials = {
         access_key_id:     ENV.fetch('S3_AWS_ACCESS_KEY_ID'),
