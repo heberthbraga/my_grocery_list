801ddf92bb43c0450ba389c03f90e9fdede02eba
Heberth Braga <braga.heberth@gmail.com>
2018-11-22 11:12:02 -0300

    Adding heroku logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,9 +75,8 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger    = Logger.new(STDOUT)
+    config.logger.level = Logger::DEBUG
   end
 
   # Do not dump schema after migrations.
