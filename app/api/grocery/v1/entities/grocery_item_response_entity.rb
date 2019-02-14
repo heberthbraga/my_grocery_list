53d1c0c0e242f022067a97b0b853034285b4de5f
Heberth Braga <heberth@quimbik.com>
2019-02-14 10:05:56 -0300

    Adding new api call to grocery item

--- app/api/grocery/v1/entities/grocery_item_response_entity.rb
+++ app/api/grocery/v1/entities/grocery_item_response_entity.rb
@@ -1,9 +1,9 @@
 module Grocery::V1::Entities
   class GroceryItemResponseEntity < Grape::Entity
     
-    expose :id,            documentation: { type: 'integer', desc: 'Grocery Item ID' }
-    expose :grocery_store, documentation: { type: 'Object' }, with: SimplifiedGroceryStoreResponseEntity
-    expose :price,         documentation: { type: 'decimal', desc: 'Grocery Item price' }
-
+    expose :id,            documentation: { type: 'integer',  desc: 'Grocery Item ID' }
+    expose :price,         documentation: { type: 'decimal',  desc: 'Grocery Item price' }
+    expose :item,          documentation: { type: 'Object' }, with: ItemResponseEntity
+    
   end
 end
